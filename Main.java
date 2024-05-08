ae728c93a098b483b1ff4c6f6ba10bbf067e4fef
alcris <jacintocrisostomo1a@gmail.com>
2024-05-08 09:18:05 -0600

    kdsjsalisdubv

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
-import usuario.Cliente;
+import usuario.utils.DatosComun;
 
-public class Main {
+public class Main{
     public static void main(String[] args) {
-        Cliente.agregarCliente();
+        
+        BankSystem bank = new BankSystem();
+        bank.ejecutarSistema();
     }
 }
